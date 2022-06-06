f8c0425467412950000cb8b1cf1c76cf56e1e62c
Tadas Simonavičius <tadas@inventi.lt>
2022-06-06 21:16:01 +0300

    1: Example with PathVariable

--- eshop/src/main/java/lt/codeacademy/eshop/helloworld/HelloWorldController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/helloworld/HelloWorldController.java
@@ -2,12 +2,14 @@ package lt.codeacademy.eshop.helloworld;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HelloWorldController {
 
-    @GetMapping("/")
-    public String hello() {
+    @GetMapping("/hello/{pathVariable}")
+    public String hello(@PathVariable String pathVariable) {
+        System.out.println("PathVariable received: " + pathVariable);
         return "hello";
     }
 }
