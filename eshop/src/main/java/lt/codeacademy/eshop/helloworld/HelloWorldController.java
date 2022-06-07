53a780dc899034eb58a6a2e476e73f301bca97bd
Tadas Simonavičius <tadas@inventi.lt>
2022-06-07 18:57:27 +0300

    2: print name from Query params

--- eshop/src/main/java/lt/codeacademy/eshop/helloworld/HelloWorldController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/helloworld/HelloWorldController.java
@@ -2,16 +2,13 @@ package lt.codeacademy.eshop.helloworld;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloWorldController {
 
-    @GetMapping("/hello/{pathVariable}")
-    public String hello(@PathVariable String pathVariable, @RequestParam String requestParam) {
-        System.out.println("PathVariable received: " + pathVariable);
-        System.out.println("RequestParam received: " + requestParam);
+    @GetMapping("/hello")
+    public String hello(@RequestParam String name) {
         return "hello";
     }
 }
