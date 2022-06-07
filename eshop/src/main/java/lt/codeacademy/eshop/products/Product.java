a2f34ef2d9826bdde9989fd38f20791ae3c98b32
Tadas Simonavičius <tadas@inventi.lt>
2022-06-07 20:54:19 +0300

    2: list products in table from hardcoded list

---
+++ eshop/src/main/java/lt/codeacademy/eshop/products/Product.java
@@ -0,0 +1,15 @@
+package lt.codeacademy.eshop.products;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+public class Product {
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+}
--- eshop/src/main/java/lt/codeacademy/eshop/products/ProductsController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/products/ProductsController.java
@@ -4,14 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import static java.util.Collections.emptyList;
+import java.math.BigDecimal;
+import java.util.Arrays;
 
 @Controller
 public class ProductsController {
 
     @GetMapping("/products")
     public String getProducts(Model model) {
-        model.addAttribute("products", emptyList());
+        model.addAttribute("products",
+                Arrays.asList(new Product("iPhone 21", "This is a product description", new BigDecimal("1230.20"))));
         return "products";
     }
 }
