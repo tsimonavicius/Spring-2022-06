822b020c3d0abed7f104416ac2596f0f703ad7d6
Tadas Simonavičius <tadas@inventi.lt>
2022-06-07 21:01:04 +0300

    2: endpoint to open product form

--- eshop/src/main/java/lt/codeacademy/eshop/products/ProductsController.java
+++ eshop/src/main/java/lt/codeacademy/eshop/products/ProductsController.java
@@ -3,17 +3,24 @@ package lt.codeacademy.eshop.products;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 
 @Controller
+@RequestMapping("/products")
 public class ProductsController {
 
-    @GetMapping("/products")
+    @GetMapping
     public String getProducts(Model model) {
         model.addAttribute("products",
                 Arrays.asList(new Product("iPhone 21", "This is a product description", new BigDecimal("1230.20"))));
         return "products";
+    }
+
+    @GetMapping("/create")
+    public String createProduct() {
+        return "productForm";
     }
 }
