256064c5fe29f30d38b6d99151ec3ad005270327
Tadas Simonavičius <tadas@inventi.lt>
2022-06-14 19:37:32 +0300

    4: init schema using hibernate and populate with initial data

--- eshop/src/main/java/lt/codeacademy/eshop/products/Product.java
+++ eshop/src/main/java/lt/codeacademy/eshop/products/Product.java
@@ -1,17 +1,25 @@
 package lt.codeacademy.eshop.products;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-@Data
+@Entity
+@Getter
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Product {
 
+    @Id
+    @Type(type = "uuid-char")
     private UUID id;
     private String name;
     private String description;
---
+++ eshop/src/main/resources/db/init.sql
@@ -0,0 +1,3 @@
+INSERT INTO PRODUCT VALUES
+    ('00000000-0000-0000-0000-000000000001', 'iPhone 23', 'Naujausias iPhone!', 5005.99),
+    ('00000000-0000-0000-0000-000000000002', 'Samsung S55', 'Naujausias Samsung!', 4999.99);
