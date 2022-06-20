6a6d0b6c23ce1980b5d7cf82adb3cd79d738e97c
Tadas Simonavičius <tadas@inventi.lt>
2022-06-20 19:38:31 +0300

    5: increase number of products

--- eshop/src/main/resources/db/init.sql
+++ eshop/src/main/resources/db/init.sql
@@ -1,3 +1,16 @@
 INSERT INTO PRODUCT (id, name, description, price) VALUES
     ('00000000-0000-0000-0000-000000000001', 'iPhone 23', 'Naujausias iPhone!', 5005.99),
-    ('00000000-0000-0000-0000-000000000002', 'Samsung S55', 'Naujausias Samsung!', 4999.99);
+    ('00000000-0000-0000-0000-000000000002', 'Samsung S55', 'Naujausias Samsung!', 4999.99),
+    ('00000000-0000-0000-0000-000000000003', 'Nokia 3310', 'Seniausia Nokia!', 10.99),
+    ('00000000-0000-0000-0000-000000000004', 'Huawei Mate', 'Kazkoks Huawei!', 15.99),
+    ('00000000-0000-0000-0000-000000000005', 'Xiaomi Mi', 'Kazkokie Mi!', 100.12),
+    ('00000000-0000-0000-0000-000000000006', 'Sony Xperia', 'Sony!!!', 1001.00),
+    ('00000000-0000-0000-0000-000000000007', 'Samsung S55', 'Naujausias Samsung!', 4999.99),
+    ('00000000-0000-0000-0000-000000000008', 'Samsung S55', 'Naujausias Samsung!', 4999.99),
+    ('00000000-0000-0000-0000-000000000009', 'Samsung S55', 'Naujausias Samsung!', 4999.99),
+    ('00000000-0000-0000-0000-000000000010', 'Samsung S55', 'Naujausias Samsung!', 4999.99),
+    ('00000000-0000-0000-0000-000000000011', 'Samsung S55', 'Naujausias Samsung!', 4999.99),
+    ('00000000-0000-0000-0000-000000000012', 'Samsung S55', 'Naujausias Samsung!', 4999.99),
+    ('00000000-0000-0000-0000-000000000013', 'Samsung S55', 'Naujausias Samsung!', 4999.99),
+    ('00000000-0000-0000-0000-000000000014', 'Samsung S55', 'Naujausias Samsung!', 4999.99)
+    ;
