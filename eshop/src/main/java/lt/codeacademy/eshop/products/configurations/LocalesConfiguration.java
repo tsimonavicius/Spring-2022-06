4594b148b9e5938ca7526046a6cae0ba3c8b2426
Tadas Simonavičius <tadas@inventi.lt>
2022-06-27 20:44:48 +0300

    7: con figure custom messages folder with MessageSource bean

--- eshop/src/main/java/lt/codeacademy/eshop/products/configurations/LocalesConfiguration.java
+++ eshop/src/main/java/lt/codeacademy/eshop/products/configurations/LocalesConfiguration.java
@@ -1,7 +1,9 @@
 package lt.codeacademy.eshop.products.configurations;
 
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -26,6 +28,15 @@ public class LocalesConfiguration implements WebMvcConfigurer {
         localeChangeInterceptor.setParamName("lang");
 
         return localeChangeInterceptor;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasename("classpath:messages/messages");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
     }
 
     @Override
