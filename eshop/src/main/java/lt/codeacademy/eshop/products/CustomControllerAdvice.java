6d1766576d75570e5700a68e08113d7826989da3
Tadas Simonavičius <tadas@inventi.lt>
2022-06-28 20:38:17 +0300

    8: create initBinder with string trimming

--- eshop/src/main/java/lt/codeacademy/eshop/products/CustomControllerAdvice.java
+++ eshop/src/main/java/lt/codeacademy/eshop/products/CustomControllerAdvice.java
@@ -1,9 +1,12 @@
 package lt.codeacademy.eshop.products;
 
 import lt.codeacademy.eshop.products.errors.CustomException;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
@@ -14,5 +17,11 @@ public class CustomControllerAdvice {
     public String handleCustomException() {
 
         return "error/customExceptionPage";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder) {
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
     }
 }
