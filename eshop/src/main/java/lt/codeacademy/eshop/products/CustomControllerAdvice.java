c7254fdd8e329d3338359d6c5a08a69b178942b9
Tadas Simonavičius <tadas@inventi.lt>
2022-06-28 20:46:54 +0300

    8: add date to model before all endpoints

--- eshop/src/main/java/lt/codeacademy/eshop/products/CustomControllerAdvice.java
+++ eshop/src/main/java/lt/codeacademy/eshop/products/CustomControllerAdvice.java
@@ -4,10 +4,9 @@ import lt.codeacademy.eshop.products.errors.CustomException;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
 
 @ControllerAdvice
 public class CustomControllerAdvice {
@@ -23,5 +22,10 @@ public class CustomControllerAdvice {
     public void initBinder(WebDataBinder webDataBinder) {
         StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
         webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
+
+    @ModelAttribute("now")
+    public Date now() {
+        return new Date();
     }
 }
