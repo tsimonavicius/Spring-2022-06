2b53b4fb0992c1f2ed24e8e7d650ab5394448eb5
Tadas Simonavičius <tadas@inventi.lt>
2022-06-28 19:21:34 +0300

    8: add custom attribute to DefaultErrorAttributes and read locale in template

---
+++ eshop/src/main/java/lt/codeacademy/eshop/products/errors/CustomErrorAttributes.java
@@ -0,0 +1,20 @@
+package lt.codeacademy.eshop.products.errors;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
+import java.util.Map;
+
+@Component
+public class CustomErrorAttributes extends DefaultErrorAttributes {
+
+    @Override
+    public Map<String, Object> getErrorAttributes(WebRequest webRequest, ErrorAttributeOptions options) {
+        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, options);
+        errorAttributes.put("customObj", "message");
+
+        return errorAttributes;
+    }
+}
