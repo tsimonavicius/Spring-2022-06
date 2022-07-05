be2b345f1a5b2b8f09185f8663a2b04f00f8e309
Tadas Simonavičius <tadas@inventi.lt>
2022-07-05 19:56:52 +0300

    10: deactivate JpaRepository bean then mocked profile is active

--- eshop/src/main/java/lt/codeacademy/eshop/products/repos/JpaProductsRepository.java
+++ eshop/src/main/java/lt/codeacademy/eshop/products/repos/JpaProductsRepository.java
@@ -1,11 +1,13 @@
 package lt.codeacademy.eshop.products.repos;
 
 import lt.codeacademy.eshop.products.Product;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Profile("!mocked-db")
 public interface JpaProductsRepository extends JpaRepository<Product, UUID>, ProductsRepository {
 
     List<Product> findByNameContainingIgnoreCase(String name);
