8e780051480c49f4208a28076e5edd2ef8a3b041
Tadas Simonavičius <tadas@inventi.lt>
2022-07-19 21:22:00 +0300

    14: add Redirect view controller for landing page

--- eshop/src/main/java/lt/codeacademy/eshop/commons/configurations/WebMvcConfiguration.java
+++ eshop/src/main/java/lt/codeacademy/eshop/commons/configurations/WebMvcConfiguration.java
@@ -56,5 +56,6 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("loginPage");
+        registry.addRedirectViewController("/", "/products");
     }
 }
