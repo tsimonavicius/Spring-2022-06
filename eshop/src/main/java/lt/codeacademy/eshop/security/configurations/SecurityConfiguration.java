164280215166d2bfcd80ee711bb7bb0e59cfc03d
Tadas Simonavičius <tadas@inventi.lt>
2022-07-19 20:41:29 +0300

    14: ignore security for static files

--- eshop/src/main/java/lt/codeacademy/eshop/security/configurations/SecurityConfiguration.java
+++ eshop/src/main/java/lt/codeacademy/eshop/security/configurations/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package lt.codeacademy.eshop.security.configurations;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
@@ -10,7 +12,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.authorizeRequests().anyRequest().authenticated();
+        http.authorizeRequests()
+                .anyRequest().authenticated();
 
         http.formLogin()
                 .permitAll()
@@ -24,5 +27,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.logout()
                 .logoutUrl("/logout")
                 .logoutSuccessUrl("/prisijungimas?logout");
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
