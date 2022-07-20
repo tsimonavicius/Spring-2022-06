022a91eb70867cbda4ec2741e0a3651bd1691f39
Tadas Simonavičius <tadas@inventi.lt>
2022-07-20 21:48:45 +0300

    14: print connected user's full name

--- eshop/src/main/java/lt/codeacademy/eshop/security/models/User.java
+++ eshop/src/main/java/lt/codeacademy/eshop/security/models/User.java
@@ -49,4 +49,8 @@ public class User implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public String getFullName() {
+        return name + " " + surname;
+    }
 }
