180f12d1eae7ff78fefba8867c183bc5d2288128
Tadas Simonavičius <tadas@inventi.lt>
2022-07-20 18:14:47 +0300

    14: disable security for H2 console

--- eshop/src/main/java/lt/codeacademy/eshop/security/configurations/SecurityConfiguration.java
+++ eshop/src/main/java/lt/codeacademy/eshop/security/configurations/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package lt.codeacademy.eshop.security.configurations;
 
+import org.springframework.boot.autoconfigure.h2.H2ConsoleProperties;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,6 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final H2ConsoleProperties h2ConsoleProperties;
+
+    public SecurityConfiguration(H2ConsoleProperties h2ConsoleProperties) {
+        this.h2ConsoleProperties = h2ConsoleProperties;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -33,6 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) {
 
         web.ignoring()
-                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .antMatchers(h2ConsoleProperties.getPath() + "/**");
     }
 }
